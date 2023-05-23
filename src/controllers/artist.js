c2b37d05f1d266e3e9ac8349d08ebfc6ff1affcd
Jenny Rafferty <raffertyjenny@gmail.com>
2023-05-23 16:02:42 +0100

    added security

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -15,4 +15,20 @@ const createArtist = async (req, res) => {
   }
 };
 
+exports.createArtist = async (req, res) => {
+  const { name, genre } = req.body;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      "INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *",
+      [name, genre]
+    );
+    res.status(201).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
 module.exports = { createArtist };
