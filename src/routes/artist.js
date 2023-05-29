2cf8a890e5c4daf5040f4cd9e984c7be9fa61818
Jenny Rafferty <raffertyjenny@gmail.com>
2023-05-29 13:27:25 +0100

    added patch

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,12 +3,13 @@ const {
   createArtist,
   getAllArtists,
   getArtistById,
+  updateArtist,
 } = require("../controllers/artist");
 
 const artistRouter = express.Router();
 
 artistRouter.route("/").post(createArtist).get(getAllArtists);
 
-artistRouter.route("/:id").get(getArtistById);
+artistRouter.route("/:id").get(getArtistById).patch(updateArtist);
 
 module.exports = artistRouter;
